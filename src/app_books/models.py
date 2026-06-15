9cd6c2981b9689cdf9e4c22e71f27d4cb60385d8
Olga Panayot <panayot.olga@gmail.com>
2026-06-15 21:14:29 +0300

    feat: add Autor, Genre and Book models

--- src/app_books/models.py
+++ src/app_books/models.py
@@ -1,3 +1,42 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    birth_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
+    genres = models.ManyToManyField(Genre, related_name="books")
+    pages = models.PositiveIntegerField()
+    language = models.CharField(
+        max_length=2, choices=[("EN", "English"), ("FR", "French"), ("ES", "Espanol")]
+    )
+    publication_date = models.DateField()
+    isbn = models.CharField(max_length=13, unique=True)
+    is_reserved = models.BooleanField(default=False)
+    reserved_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="reserved_books",
+    )
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="books")
+
+    def __str__(self):
+        return self.title
