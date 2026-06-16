dc94a98321341a09b30ab14818c888d8c1645521
Olga Panayot <panayot.olga@gmail.com>
2026-06-16 18:19:03 +0300

    feat: add IsOwnerOrReadOnly and IsAdminOrReadOnly permissions

---
+++ src/app_books/permissions.py
@@ -0,0 +1,24 @@
+from rest_framework.permissions import SAFE_METHODS, BasePermission
+
+
+class IsOwnerOrReadOnly(BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+
+        if request.method in SAFE_METHODS:
+            return True
+
+        if request.user.is_superuser:
+            return True
+
+        return obj.owner == request.user
+
+
+class IsAdminOrReadOnly(BasePermission):
+
+    def has_permission(self, request, view):
+
+        if request.method in SAFE_METHODS:
+            return True
+
+        return request.user and request.user.is_staff
